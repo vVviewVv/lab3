5779ff698074dc26b44e043113ea1f9b010a6a73
vVviewVv <viewissara1246@gmail.com>
2019-12-28 14:08:30 +0700

    work

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a,b
-    cout << "AAAAA";
+int main() {
+    float a = 1;
+    float b = 2;
+    float c;
+    float ans = 0;
+
+    while(b <= 69){
+        c = a/b;
+        ans = ans+c;
+        b = b+1;
+    }
+    
+    cout << "ans=" << ans;
+
     return 0;
 }
